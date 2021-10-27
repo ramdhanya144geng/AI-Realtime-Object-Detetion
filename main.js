4df47f5e96b769434450cfbd861ff3db3ebbd4ac
ramdhanya144geng <93275664+ramdhanya144geng@users.noreply.github.com>
2021-10-27 21:39:17 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+status = "";
+
+function preload(){
+    video = createVideo();
+    video.hide();
+}
+
+function setup(){
+    canvas = createCanvas(380, 380);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(380, 380);
+    video.hide();
+}
+
+function draw(){
+    image(0, 0, 380, 380);
+}
+
+function start(){
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
+
+function modelLoaded(){
+    console.log("Model Loaded!");
+    status = true;
+    video.loop();
+    video.speed(1);
+    video.volume(0);
+}
